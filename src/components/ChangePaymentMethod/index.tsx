a631e9c56bfa7c2197b2b4960c7fd5667955795b
Vlad Beltsar <vladyslavbeltsar@gmail.com>
2021-09-24 16:47:49 +0300

    HY-140 | Added CardVariant component and modal for it

--- src/components/ChangePaymentMethod/index.tsx
+++ src/components/ChangePaymentMethod/index.tsx
@@ -10,9 +10,14 @@ import i18n from 'i18n';
 interface IChangePaymentMethodProps {
   theme?: ProjectThemeType;
   addCard: () => void;
+  changeCard: () => void;
 }
 
-const ChangePaymentMethod: React.FC<IChangePaymentMethodProps> = ({ theme, addCard }) => {
+const ChangePaymentMethod: React.FC<IChangePaymentMethodProps> = ({
+  theme,
+  addCard,
+  changeCard,
+}) => {
   const styles = useStyles(theme);
 
   const { close } = useBottomSheet();
@@ -20,16 +25,23 @@ const ChangePaymentMethod: React.FC<IChangePaymentMethodProps> = ({ theme, addCa
     close();
     addCard();
   };
+  const handleChangeCard = () => {
+    close();
+    changeCard();
+  };
+
   return (
     <View>
       <Text style={styles.title}>{i18n.t('components.changePaymentMethod.title')}</Text>
-      <View style={styles.optionContainer}>
-        <View style={styles.option}>
-          <CreditCard />
-          <Text style={styles.optionText}>{i18n.t('components.changePaymentMethod.changeCard')}</Text>
+      <Pressable onPress={handleChangeCard}>
+        <View style={styles.optionContainer}>
+          <View style={styles.option}>
+            <CreditCard />
+            <Text style={styles.optionText}>{i18n.t('components.changePaymentMethod.changeCard')}</Text>
+          </View>
+          <LeftChevron />
         </View>
-        <LeftChevron />
-      </View>
+      </Pressable>
       <Pressable onPress={handleAddNewCard}>
         <View style={[styles.optionContainer, styles.bottomOptionContainer]}>
           <View style={styles.option}>
