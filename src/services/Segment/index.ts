cd33104b642a18d4f871249d61531344f9f5a5c3
Vlad Beltsar <vladyslavbeltsar@gmail.com>
2021-11-17 12:01:31 +0200

    HY-387 | Added tracking status to segment for ios

--- src/services/Segment/index.ts
+++ src/services/Segment/index.ts
@@ -1,8 +1,11 @@
 import analytics from '@segment/analytics-react-native';
+import { getTrackingStatus } from 'react-native-tracking-transparency';
 
-export const setupSegment = () => {
-  if (!__DEV__) {
-    analytics.setup('AwLaHfx4u5NxVbWUBF88qcBvvK1geWK9', {
+export const setupSegment = async () => {
+  const trackingStatus = await getTrackingStatus();
+
+  if (!__DEV__ && (trackingStatus === 'authorized' || trackingStatus === 'unavailable')) {
+    await analytics.setup('AwLaHfx4u5NxVbWUBF88qcBvvK1geWK9', {
       trackAppLifecycleEvents: true,
     });
   }
