7811f7310d155fdb0034ee80fa0358ae475d0f8d
Pavel Lapin <44573208+pavel-stryber@users.noreply.github.com>
2022-03-21 16:32:35 +0200

    HY-793 FE Update delivery fees to 4 AED (#240)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const SERVER_DATE_FORMAT = 'YYYY-MM-DD[T]HH:mm:ss.000000';
 export const HEADER_HEIGHT = 95;
 export const TAB_BAR_HEIGHT = 75;
-export const DELIVERY_FEE = 8;
+export const DELIVERY_FEE = 4;
 export const BASKET_TAB_NAME = 'BASKET_TAB_NAME';
 export const storageKeys = {
   boardingCompleted: 'boardingCompleted',
